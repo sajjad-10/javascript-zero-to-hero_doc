4afed41673fe975aa19d49458ac4e6ea29ae6773
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-02-03 19:08:51 +0330

    game gravity

--- canvas/canvas-game-gravity.js
+++ canvas/canvas-game-gravity.js
@@ -19,11 +19,13 @@ class Ball {
         this.gravity = 1;
         this.friction = 0.8;
         this.r = r || 20;
-        this.x = x || randomIntFromInterval(0 + this.r, window.innerWidth - this.r);
-        this.y = y || randomIntFromInterval(0 + this.r, window.innerHeight - this.r);
+        this.x =
+            x || randomIntFromInterval(0 + this.r, window.innerWidth - this.r);
+        this.y =
+            y || randomIntFromInterval(0 + this.r, window.innerHeight - this.r);
         this.dx = dx || (Math.random() - 0.5) * 20; // -0.5   +0.5
-        this.dy = dy || (Math.random()) * 4; // for speed up to 10
-        this.color = color || `rgba(231, 76, 60,${Math.random()})`
+        this.dy = dy || Math.random() * 4; // for speed up to 10
+        this.color = color || `rgba(231, 76, 60,${Math.random()})`;
         this.draw();
     }
     draw() {
@@ -39,9 +41,12 @@ class Ball {
         } else {
             this.dy += this.gravity;
         }
-        if (this.x + this.r + this.dx >= screen.width||this.x + this.r + this.dx <= 0 ) {
+        if (
+            this.x + this.r + this.dx >= screen.width ||
+            this.x + this.r + this.dx <= 0
+        ) {
             this.dx = -this.dx;
-        } 
+        }
         this.y += this.dy;
         this.x += this.dx;
         this.draw();
@@ -54,7 +59,7 @@ class Canvas {
             this.balls.push(new Ball());
         }
     }
-    
+
     animate() {
         c.clearRect(0, 0, window.innerWidth, window.innerHeight); // for remove prev
         this.balls.forEach((ball) => {
@@ -67,16 +72,6 @@ let mycan = new Canvas();
 mycan.animate();
 
 window.addEventListener("mousemove", (e) => {
-    // balls.forEach((ball) => {
-    //     let distance = Math.sqrt(
-    //         Math.pow(e.clientX - ball.x, 2) + Math.pow(e.clientY - ball.y, 2)
-    //     ); // Obtaining distance in two-dimensional space
-    //     if (distance < 100 && ball.r < ball.baseR * 4) {
-    //         ball.r += 2;
-    //     } else if (ball.r > ball.baseR) {
-    //         ball.r -= 2;
-    //     }
-    // });
     mouse.x = e.clientX;
     mouse.y = e.clientY;
 });
